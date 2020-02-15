454f87ad6c400c5e198f65bdf4953b077e8b7442
David Barroso <dbarrosop@dravetech.com>
2020-02-15 11:18:11 +0100

    progress

---
+++ tests/unit/test_napalm_configure.py
@@ -0,0 +1,64 @@
+import os
+
+from napalm.base import exceptions
+
+from nornir_napalm.tasks import napalm_configure
+from nornir_napalm.connections import CONNECTION_NAME
+
+
+THIS_DIR = os.path.dirname(os.path.realpath(__file__)) + "/mocked/napalm_configure"
+
+
+def connect(task, extras):
+    if CONNECTION_NAME in task.host.connections:
+        task.host.close_connection(CONNECTION_NAME)
+    task.host.open_connection(
+        CONNECTION_NAME,
+        task.nornir.config,
+        extras={"optional_args": extras},
+        default_to_host_attributes=True,
+    )
+
+
+class Test(object):
+    def test_napalm_configure_change_dry_run(self, nornir):
+        opt = {"path": THIS_DIR + "/test_napalm_configure_change_dry_run"}
+        configuration = "hostname changed-hostname"
+        d = nornir.filter(name="dev3.group_2")
+        d.run(connect, extras=opt)
+        result = d.run(napalm_configure, dry_run=True, configuration=configuration)
+        assert result
+        for h, r in result.items():
+            assert "+hostname changed-hostname" in r.diff
+            assert r.changed
+
+    def test_napalm_configure_change_commit(self, nornir):
+        opt = {"path": THIS_DIR + "/test_napalm_configure_change_commit/step1"}
+        configuration = "hostname changed-hostname"
+        d = nornir.filter(name="dev3.group_2")
+        d.run(connect, extras=opt)
+        result = d.run(napalm_configure, dry_run=False, configuration=configuration)
+        assert result
+        for h, r in result.items():
+            assert "+hostname changed-hostname" in r.diff
+            assert r.changed
+        opt = {"path": THIS_DIR + "/test_napalm_configure_change_commit/step2"}
+        d.run(connect, extras=opt)
+        result = d.run(napalm_configure, dry_run=True, configuration=configuration)
+        assert result
+        for h, r in result.items():
+            assert "+hostname changed-hostname" not in r.diff
+            assert not r.changed
+
+    def test_napalm_configure_change_error(self, nornir):
+        opt = {"path": THIS_DIR + "/test_napalm_configure_change_error"}
+        configuration = "hostname changed_hostname"
+
+        d = nornir.filter(name="dev3.group_2")
+        d.run(connect, extras=opt)
+        results = d.run(napalm_configure, configuration=configuration)
+        processed = False
+        for result in results.values():
+            processed = True
+            assert isinstance(result.exception, exceptions.MergeConfigException)
+        assert processed
---
+++ tests/unit/test_napalm_validate.py
@@ -0,0 +1,54 @@
+import os
+
+from nornir_napalm.tasks import napalm_validate
+from nornir_napalm.connections import CONNECTION_NAME
+
+
+THIS_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+def connect(task, extras):
+    if CONNECTION_NAME in task.host.connections:
+        task.host.close_connection(CONNECTION_NAME)
+    task.host.open_connection(
+        CONNECTION_NAME,
+        task.nornir.config,
+        extras={"optional_args": extras},
+        default_to_host_attributes=True,
+    )
+
+
+class Test(object):
+    def test_napalm_validate_src_ok(self, nornir):
+        opt = {"path": THIS_DIR + "/mocked/napalm_get/test_napalm_getters"}
+        d = nornir.filter(name="dev3.group_2")
+        d.run(connect, extras=opt)
+        result = d.run(napalm_validate, src=THIS_DIR + "/data/validate_ok.yaml")
+        assert result
+        for h, r in result.items():
+            assert not r.failed
+
+    def test_napalm_validate_src_error(self, nornir):
+        opt = {"path": THIS_DIR + "/mocked/napalm_get/test_napalm_getters"}
+        d = nornir.filter(name="dev3.group_2")
+        d.run(connect, extras=opt)
+
+        result = d.run(napalm_validate, src=THIS_DIR + "/data/validate_error.yaml")
+        assert result
+        for h, r in result.items():
+            assert not r.failed
+            assert not r.result["complies"]
+
+    def test_napalm_validate_src_validate_source(self, nornir):
+        opt = {"path": THIS_DIR + "/mocked/napalm_get/test_napalm_getters"}
+        d = nornir.filter(name="dev3.group_2")
+        d.run(connect, extras=opt)
+
+        validation_dict = [{"get_interfaces": {"Ethernet1": {"description": ""}}}]
+
+        result = d.run(napalm_validate, validation_source=validation_dict)
+
+        assert result
+        for h, r in result.items():
+            assert not r.failed
+            assert r.result["complies"]
