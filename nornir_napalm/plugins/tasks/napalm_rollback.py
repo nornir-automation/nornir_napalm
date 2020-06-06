d1a1ec79ac028748e35a23fa36ebc25eec7714ab
Jay Christopher <jaychristopher327@gmail.com>
2020-06-06 11:17:39 -0700

    import CONNECTION_NAME in napalm_rollback.py

--- nornir_napalm/plugins/tasks/napalm_rollback.py
+++ nornir_napalm/plugins/tasks/napalm_rollback.py
@@ -1,5 +1,6 @@
 from nornir.core.task import Result, Task
 
+from nornir_napalm.plugins.connections import CONNECTION_NAME
 
 def napalm_rollback(task: Task, dry_run: bool = None) -> Result:
     """
@@ -7,7 +8,7 @@ def napalm_rollback(task: Task, dry_run: bool = None) -> Result:
     Arguments:
         dry_run: whether to rollback the configuration or not
     """
-    device = task.host.get_connection("napalm", task.nornir.config)
+    device = task.host.get_connection(CONNECTION_NAME, task.nornir.config)
 
     if not dry_run:
         device.rollback()
