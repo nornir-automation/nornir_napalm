03c779ddb1cd17e504128655cdf05bc37f1250b1
Jay Christopher <jaychristopher327@gmail.com>
2020-06-06 12:11:50 -0700

    fix pylama error on napalm_rollback

--- nornir_napalm/plugins/tasks/napalm_rollback.py
+++ nornir_napalm/plugins/tasks/napalm_rollback.py
@@ -2,6 +2,7 @@ from nornir.core.task import Result, Task
 
 from nornir_napalm.plugins.connections import CONNECTION_NAME
 
+
 def napalm_rollback(task: Task, dry_run: bool = None) -> Result:
     """
     Rollback device configuration using napalm
