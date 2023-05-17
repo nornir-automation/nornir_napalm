4236db5eb4e73d6a872238cdb1b915a05a1cfcd6
sigurroot <jasonrob@uvic.ca>
2023-05-17 10:00:42 -0700

    pass encoding to napalm_cli or eos

--- nornir_napalm/plugins/tasks/napalm_cli.py
+++ nornir_napalm/plugins/tasks/napalm_cli.py
@@ -5,7 +5,7 @@ from nornir.core.task import Result, Task
 from nornir_napalm.plugins.connections import CONNECTION_NAME
 
 
-def napalm_cli(task: Task, commands: List[str]) -> Result:
+def napalm_cli(task: Task, commands: List[str], encoding: 'text') -> Result:
     """
     Run commands on remote devices using napalm
 
@@ -17,5 +17,8 @@ def napalm_cli(task: Task, commands: List[str]) -> Result:
         * result (``dict``): result of the commands execution
     """
     device = task.host.get_connection(CONNECTION_NAME, task.nornir.config)
-    result = device.cli(commands)
+    if task.host.platform == 'eos':
+      result = device.cli(commands, encoding=encoding)
+    else:
+      result = device.cli(command)
     return Result(host=task.host, result=result)
