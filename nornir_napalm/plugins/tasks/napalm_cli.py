9c1941edca2ec5cb33750aa39688a918c87c0eff
siguroot <133690336+siguroot@users.noreply.github.com>
2023-05-22 15:51:08 -0700

    Support encoding in napalm_cli (and future cli arguments)

--- nornir_napalm/plugins/tasks/napalm_cli.py
+++ nornir_napalm/plugins/tasks/napalm_cli.py
@@ -1,21 +1,21 @@
-from typing import List
+from typing import Any, List
 
 from nornir.core.task import Result, Task
 
 from nornir_napalm.plugins.connections import CONNECTION_NAME
 
 
-def napalm_cli(task: Task, commands: List[str]) -> Result:
+def napalm_cli(task: Task, commands: List[str], **kwargs: Any) -> Result:
     """
     Run commands on remote devices using napalm
 
     Arguments:
-      commands: commands to execute
-
+    commands: commands to execute
+      **kwargs: placeholder for user added arguments
     Returns:
       Result object with the following attributes set:
         * result (``dict``): result of the commands execution
     """
     device = task.host.get_connection(CONNECTION_NAME, task.nornir.config)
-    result = device.cli(commands)
+    result = device.cli(commands, **kwargs)
     return Result(host=task.host, result=result)
